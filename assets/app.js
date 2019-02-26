a113f1894c556125b2ae180b8cc0ddaafba081c2
SPFlores <sflores92886@gmail.com>
2019-02-25 21:25:28 -0800

    footer

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+// this is the javascript I've found that is somehow supposed to help me make the navbar collapse/uncollapse, but it's not working out so well for me
 $(document).ready(function () {
   $(".button-collapse").sideNav();
 });
