27113bdf3b337780b76275e708fec936f0f9732a
SPFlores <sflores92886@gmail.com>
2019-02-26 17:50:03 -0800

    more nav bar tweaks

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,13 @@
 // this is the javascript I've found that is somehow supposed to help me make the navbar collapse/uncollapse, but it's not working out so well for me
+// $(document).ready(function () {
+//   $(".button-collapse").sideNav();
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
 $(document).ready(function () {
   $(".button-collapse").sideNav();
 });
